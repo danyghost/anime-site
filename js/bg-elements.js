801c0c422d338167dda4bc1607586cd11dd588a9
danyghost <114016666+danyghost@users.noreply.github.com>
2024-02-29 19:52:54 +0300

    Update bg-elements.js

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -1,13 +1,9 @@
 const bgElements = () => {
 	const elements = document.querySelectorAll(".set-bg")
 
-	console.log(elements);
-	for (let i = 0; i < elements.length; i++) {
-		console.dir(elements[i]);
-		const src = elements[i].dataset.setbg
-
-		elements[i].style.backgroundImage = `url(${src})`
-	}
+	elements.forEach((elem) => {
+		elem.style.backgroundImage = `url(${elem.dataset.setbg})`
+	})
 }
 
 bgElements()
